8ae1ef7928964ef2eafcedf133e38ac7b044695b
lebansty <lebanstyv@gmail.com>
2022-09-28 12:54:41 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Card from './Card';
+import Nav from './Nav';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Cart from './Cart';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Nav/>}>
+
+      <Route index element={<Card/>}></Route>
+      <Route path='cart' element={<Cart/>}></Route>
+        </Route>
+      </Routes>
+      
+      </BrowserRouter>
+     
     </div>
   );
 }
